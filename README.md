642e63bf3d14835e4a4fdd0903839e0935a1ad8e
Mark Guildea <mark.anthony.guildea@gmail.com>
2019-01-09 23:19:08 +0000

    first simple game

---
+++ README.md
@@ -0,0 +1,7 @@
+Games developed with JS
+
+Mainly Udemy course material
+
+https://www.udemy.com/user/lars51/
+
+This guy knows his stuff
---
+++ main.js
@@ -0,0 +1,5 @@
+const message = document.querySelector(".message");
+const buttons = document.querySelectorAll("button");
+console.log(buttons);
+
+
